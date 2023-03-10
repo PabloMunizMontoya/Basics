c1591b433f3eaf8bfbeb2f5e984cb51e29b1fa9b
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-09 20:41:13 -0500

    Re utilizable component with props

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,10 +1,31 @@
 import React, {
-  Fragment
+  Fragment, useState
 } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer';
+import Product from './components/Product';
 
 function App() {
+
+  //create product list
+    const [ products, setProducts] = useState([
+      { id: 1,
+        name: 'T-Shirt ReactJS', 
+        price: 10
+      },
+      { id: 2,
+        name: 'T-Shirt VueJS', 
+        price: 12
+      },
+      { id: 3,
+        name: 'T-Shirt NodeJS', 
+        price: 10
+      },
+      { id: 4,
+        name: 'T-Shirt Angular', 
+        price: 15
+      },
+    ])
 
   //Obtain the date
   const date = new Date().getFullYear()
@@ -14,6 +35,14 @@ function App() {
         <Header
           title = 'Virtual Shop'
         />
+
+        <h1>List of products</h1>
+        {products.map(product => (
+          <Product
+            key={product.id}
+            product = {product}
+          />
+        ))}
 
         <Footer
           date = {date}
